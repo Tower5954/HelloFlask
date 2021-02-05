9c4c6681b9812fe16e9cf10f9d09cbe27828cd2e
Tower59 <g.thompson59@live.co.uk>
2021-02-05 04:02:15 +0000

    Initial commit

---
+++ file_one.py
@@ -0,0 +1,12 @@
+import file_two
+
+
+# Python file one module
+
+print("File one __name__ is set to: {}" .format(__name__))
+
+
+if __name__ == "__main__":
+   print("File one executed when ran directly")
+else:
+   print("File one executed when imported")
