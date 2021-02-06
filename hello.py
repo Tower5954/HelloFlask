d6c80d2f33c7f86c5215e2c9b38ffbb34d76c5ab
Tower59 <g.thompson59@live.co.uk>
2021-02-06 03:51:00 +0000

    second commit?

--- hello.py
+++ hello.py
@@ -2,9 +2,22 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return "<h1>Hello, World!</h1>"
+
+
+@app.route("/bye")
+def bye():
+    return "Bye!"
+
+
+@app.route("/<name>")
+def greet(name):
+    return f"Hello there {name}"
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
+
